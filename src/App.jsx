b51c8110fef8dc574e226f49337a9432389eb2f9
bhawna930 <bhawnasharma5779@gmail.com>
2025-05-05 11:37:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    
+      <div>
+        <h1>WELCOME TO REACT WITH IMPLEMENT CI-CD PIPELINE </h1>
+        <h2>BY BHAWNA</h2>
+      </div>
+   
+  )
+}
+
+export default App
